5b242a2067078eab9f011defe9483beaed7c7086
Crisan Cristina <ccrisan@copyright.com>
2022-09-22 16:06:17 +0300

    Convert map component

--- app/components/map.ts
+++ app/components/map.ts
@@ -1,11 +1,21 @@
 import Component from '@glimmer/component';
 import ENV from 'super-rentals/config/environment';
+import { MapArgs } from 'super-rentals/interfaces/map-args';
 
 const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
 
-export default class MapComponent extends Component {
-  get src() {
-    let { lng, lat, width, height, zoom } = this.args;
+export default class MapComponent extends Component<MapArgs> {
+  get lng(): number | undefined {
+    return this.args.lng;
+  }
+
+  get lat(): number | undefined{
+    return this.args.lat;
+  }
+
+  get src(): string {
+    let { width, height, zoom } = this.args;
+    let { lng, lat } = this;
 
     let coordinates = `${lng},${lat},${zoom}`;
     let dimensions = `${width}x${height}`;
@@ -14,7 +24,7 @@ export default class MapComponent extends Component {
     return `${MAPBOX_API}/${coordinates}/${dimensions}@2x?${accessToken}`;
   }
 
-  get token() {
+  get token(): string {
     return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
   }
 }
---
+++ app/interfaces/map-args.ts
@@ -0,0 +1,7 @@
+export interface MapArgs {
+    lng: number | undefined;
+    lat: number | undefined;
+    width: string;
+    height: string;
+    zoom: string;
+  }
